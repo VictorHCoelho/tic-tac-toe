2f275231b3299673495995e1687a5245ed78700d
Victor Hugo Coelho <victorhugocoelho2000@gmail.com>
2022-05-18 00:52:48 -0300

    Almost finish

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ let mainContainer = document.getElementById('main-container');
 
 let turn = document.getElementById('vez-jogar');
 
+
 btnStart.addEventListener('click', startClick);
 btnRestart.addEventListener('click', restartClick);
 pos1.addEventListener('click', item1);
@@ -26,14 +27,23 @@ pos7.addEventListener('click', item7);
 pos8.addEventListener('click', item8);
 pos9.addEventListener('click', item9);
 
+pos1.addEventListener('click', results);
+pos2.addEventListener('click', results);
+pos3.addEventListener('click', results);
+pos4.addEventListener('click', results);
+pos5.addEventListener('click', results);
+pos6.addEventListener('click', results);
+pos7.addEventListener('click', results);
+pos8.addEventListener('click', results);
+pos9.addEventListener('click', results);
+
 function startClick() {
     btnStart.style.display = 'none';
     mainContainer.style.display = 'flex';
 }
 
 function restartClick() {
-    btnStart.style.display = 'block';
-    mainContainer.style.display = 'none';
+    location.reload();
 }
 
 function item1() {
@@ -163,3 +173,153 @@ function item9() {
     }
 }
 
+function results() {
+
+    // Horizontal
+    if ((pos1.value == 'x' || pos1.value == 'X') && (pos2.value == 'x' || pos2.value == 'X') && (pos3.value == 'x' || pos3.value == 'X')) {
+        pos4.disabled = true;
+        pos5.disabled = true;
+        pos6.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('X venceu');
+    } else if ((pos4.value == 'x' || pos4.value == 'X') && (pos5.value == 'x' || pos5.value == 'X') && (pos6.value == 'x' || pos6.value == 'X')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('X venceu');
+    } else if ((pos7.value == 'x' || pos7.value == 'X') && (pos8.value == 'x' || pos8.value == 'X') && (pos9.value == 'x' || pos9.value == 'X')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos4.disabled = true;
+        pos5.disabled = true;
+        pos6.disabled = true;
+        window.alert('X venceu');
+
+    // Diagonal
+    } else if ((pos1.value == 'x' || pos1.value == 'X') && (pos4.value == 'x' || pos4.value == 'X') && (pos7.value == 'x' || pos7.value == 'X')) {
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos5.disabled = true;
+        pos6.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('X venceu');
+    } else if ((pos3.value == 'x' || pos3.value == 'X') && (pos5.value == 'x' || pos5.value == 'X') && (pos7.value == 'x' || pos7.value == 'X')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos4.disabled = true;
+        pos6.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('X venceu');
+
+    // Vertical
+    } else if ((pos1.value == 'x' || pos1.value == 'X') && (pos5.value == 'x' || pos5.value == 'X') && (pos9.value == 'x' || pos9.value == 'X')) {
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos4.disabled = true;
+        pos6.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        window.alert('X venceu');
+    } else if ((pos2.value == 'x' || pos2.value == 'X') && (pos5.value == 'x' || pos5.value == 'X') && (pos8.value == 'x' || pos8.value == 'X')) {
+        pos1.disabled = true;
+        pos3.disabled = true;
+        pos4.disabled = true;
+        pos6.disabled = true;
+        pos7.disabled = true;
+        pos9.disabled = true;
+        window.alert('X venceu');
+    } else if ((pos3.value == 'x' || pos3.value == 'X') && (pos6.value == 'x' || pos6.value == 'X') && (pos9.value == 'x' || pos9.value == 'X')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos4.disabled = true;
+        pos5.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        window.alert('X venceu');
+    } 
+    
+
+    // Horizontal
+    if ((pos1.value == 'o' || pos1.value == 'O') && (pos2.value == 'o' || pos2.value == 'O') && (pos3.value == 'o' || pos3.value == 'O')) {
+        pos4.disabled = true;
+        pos5.disabled = true;
+        pos6.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('O venceu');
+    } else if ((pos4.value == 'o' || pos4.value == 'O') && (pos5.value == 'o' || pos5.value == 'O') && (pos6.value == 'o' || pos6.value == 'O')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('O venceu');
+    } else if ((pos7.value == 'o' || pos7.value == 'O') && (pos8.value == 'o' || pos8.value == 'O') && (pos9.value == 'o' || pos9.value == 'O')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos4.disabled = true;
+        pos5.disabled = true;
+        pos6.disabled = true;
+        window.alert('O venceu');
+
+    // Diagonal
+    } else if ((pos1.value == 'o' || pos1.value == 'O') && (pos4.value == 'o' || pos4.value == 'O') && (pos7.value == 'o' || pos7.value == 'O')) {
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos5.disabled = true;
+        pos6.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('O venceu');
+    } else if ((pos3.value == 'o' || pos3.value == 'O') && (pos5.value == 'o' || pos5.value == 'O') && (pos7.value == 'o' || pos7.value == 'O')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos4.disabled = true;
+        pos6.disabled = true;
+        pos8.disabled = true;
+        pos9.disabled = true;
+        window.alert('O venceu');
+
+    // Vertical
+    } else if ((pos1.value == 'o' || pos1.value == 'O') && (pos5.value == 'o' || pos5.value == 'O') && (pos9.value == 'o' || pos9.value == 'O')) {
+        pos2.disabled = true;
+        pos3.disabled = true;
+        pos4.disabled = true;
+        pos6.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        window.alert('O venceu');
+    } else if ((pos2.value == 'o' || pos2.value == 'O') && (pos5.value == 'o' || pos5.value == 'O') && (pos8.value == 'o' || pos8.value == 'O')) {
+        pos1.disabled = true;
+        pos3.disabled = true;
+        pos4.disabled = true;
+        pos6.disabled = true;
+        pos7.disabled = true;
+        pos9.disabled = true;
+        window.alert('O venceu');
+    } else if ((pos3.value == 'o' || pos3.value == 'O') && (pos6.value == 'o' || pos6.value == 'O') && (pos9.value == 'o' || pos9.value == 'O')) {
+        pos1.disabled = true;
+        pos2.disabled = true;
+        pos4.disabled = true;
+        pos5.disabled = true;
+        pos7.disabled = true;
+        pos8.disabled = true;
+        window.alert('O venceu');
+    } else if ((pos1.value == 'O' || pos1.value == 'X') && (pos2.value == 'O' || pos2.value == 'X') && (pos3.value == 'O' || pos3.value == 'X') &&
+              (pos4.value == 'O' || pos4.value == 'X') && (pos5.value == 'O' || pos5.value == 'X') && (pos6.value == 'O' || pos6.value == 'X') &&
+              (pos7.value == 'O' || pos7.value == 'X') && (pos8.value == 'O' || pos8.value == 'X') && (pos9.value == 'O' || pos9.value == 'X')) {                  
+        window.alert('Deu velha');
+    }
+
+}
